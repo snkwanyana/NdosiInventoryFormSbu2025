c85f234ef06ba2831cc13a09f03387880e9c337e
Administrator <sibusiso.nkwanyana@simplify.africa>
2025-11-20 22:13:03 +0200

    moved base class to utils package

--- src/test/java/utils/Base.java
+++ src/test/java/utils/Base.java
@@ -1,7 +1,8 @@
-package base;
+package utils;
 
 import org.openqa.selenium.WebDriver;
 import pagesObjects.HomePage;
+import pagesObjects.LearningMaterialsPage;
 import utils.BrowserFactory;
 
 public class Base {
@@ -10,4 +11,5 @@ public class Base {
 
     final WebDriver driver = browserFactory.startBrowser("chrome", "https://www.ndosiautomation.co.za/");
     HomePage homePage = new HomePage(driver);
+    LearningMaterialsPage learningMaterialsPage = new LearningMaterialsPage(driver);
 }
