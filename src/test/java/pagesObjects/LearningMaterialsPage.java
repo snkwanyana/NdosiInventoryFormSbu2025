8dfd2d8fe7c02ca42b61f779e5c3777ca40698aa
Administrator <sibusiso.nkwanyana@simplify.africa>
2025-11-20 22:10:14 +0200

    defined elements with their methods

--- src/test/java/pagesObjects/LearningMaterialsPage.java
+++ src/test/java/pagesObjects/LearningMaterialsPage.java
@@ -1,4 +1,49 @@
 package pagesObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LearningMaterialsPage {
+
+    WebDriver driver;
+
+    @FindBy(xpath = "//span[contains(text(),'Access Learning Materials')]")
+    WebElement loginScreenXpath;
+    @FindBy(id = "login-email")
+    WebElement emailFieldId;
+
+    @FindBy(id = "login-password")
+    WebElement passwordFieldId;
+
+    @FindBy(id = "login-submit")
+    WebElement loginButtonId;
+
+    @FindBy(id = "signup-toggle")
+    WebElement signupButtonId;
+
+    public LearningMaterialsPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyLoginScreenIsDisplayed() {
+        loginScreenXpath.isDisplayed();
+    }
+
+    public void enterEmail(String email) {
+        emailFieldId.sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        passwordFieldId.sendKeys(password);
+    }
+
+    public void clickOnLoginButton() {
+        loginButtonId.click();
+    }
+
+    public void clickOnSignupButton() {
+        signupButtonId.click();
+    }
+
 }
