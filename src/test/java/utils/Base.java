e46b04fe10fd5e870e10feaf6f0b07cbc970bdf8
Administrator <sibusiso.nkwanyana@simplify.africa>
2025-11-22 22:59:02 +0200

    added testng.xml file to run all the test classes

--- src/test/java/utils/Base.java
+++ src/test/java/utils/Base.java
@@ -11,12 +11,14 @@ public class Base {
 
     static BrowserFactory browserFactory = new BrowserFactory();
 
-    static final WebDriver driver = browserFactory.startBrowser("chrome", "https://www.ndosiautomation.co.za/");
+    public static final WebDriver driver = browserFactory.startBrowser("chrome", "https://www.ndosiautomation.co.za/");
     public HomePage homePage = PageFactory.initElements(driver, HomePage.class);
     public LearningMaterialsPage learningMaterialsPage = PageFactory.initElements(driver, LearningMaterialsPage.class);
 
+
     @AfterClass
-    public void tearDown() {
+    public static void tearDown() {
+        // Close the browser after all tests in this class
         if (driver != null) {
             driver.quit();
         }
